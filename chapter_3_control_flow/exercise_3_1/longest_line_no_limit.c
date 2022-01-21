6fd274e356238e06b7ea78bfec128ea936e7c63b
Blazej <blazej.marciniak@deepsense.ai>
2022-01-21 17:22:31 +0100

    Add Exercise 3 1

---
+++ chapter_3_control_flow/exercise_3_1/longest_line_no_limit.c
@@ -0,0 +1,20 @@
+int binary_search(int x, int v[], int n) {
+    int low, high, mid;
+    low = 0;
+    high = n - 1;
+    mid = (low + high) / 2;
+
+    while (low <= high && x != v[mid]) {
+        if (x < v[mid]) {
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+        mid = (low + high) / 2;
+    }
+    if (x == v[mid]) {
+        return mid;
+    } else {
+        return -1;
+    }
+}
